33564f229cdefcfbbb5fc408c69ac23599ded60e
unknown <kennataddese6@gmail.com>
2024-07-17 14:10:54 -0700

    enhance hero

--- app/ui/hero.tsx
+++ app/ui/hero.tsx
@@ -6,15 +6,13 @@ export default function Hero() {
   return (
     <div className="heroContainer col-xl-12 col-xxl-9">
       <div className=" col-lg-6 herotextcontainer">
-        <h1 className="my-2">
-          Empowering health care with quality medical equipments
-        </h1>
-        <p className="my-2">
+        <h1 className="my-2">Empowering health care with quality</h1>
+        <p className="my-2 text-secondary">
           We are dedicated to enhancing healthcare by providing top-tier medical
           equipment from around the globe.Our commitment to excellence ensures
           that healthcare providers have access to the latest reliable tools.
         </p>
-        <button className="btn btn-primary my-2 w-25">Learn more</button>
+        <button className="btn btn-primary my-2 w-25 py-2">Learn more</button>
       </div>
       <div className="col-lg-6 ">
         <Image src={heroImage} alt="Hero Image" className="hero-image" />
