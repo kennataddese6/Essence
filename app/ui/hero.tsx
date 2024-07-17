6dbbe09480b3e0997ac6d74be71aabe4638f65e8
unknown <kennataddese6@gmail.com>
2024-07-17 13:51:45 -0700

    add hero

--- app/ui/hero.tsx
+++ app/ui/hero.tsx
@@ -1,7 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import heroImage from '@/assets/heroImage.png';
+import Image from 'next/image';
 export default function Hero() {
   return (
     <div className="heroContainer col-xl-12 col-xxl-9">
-      <h1>Hero page</h1>
+      <div className=" col-lg-6 herotextcontainer">
+        <h1 className="my-2">
+          Empowering health care with quality medical equipments
+        </h1>
+        <p className="my-2">
+          We are dedicated to enhancing healthcare by providing top-tier medical
+          equipment from around the globe.Our commitment to excellence ensures
+          that healthcare providers have access to the latest reliable tools.
+        </p>
+        <button className="btn btn-primary my-2 w-25">Learn more</button>
+      </div>
+      <div className="col-lg-6 ">
+        <Image src={heroImage} alt="Hero Image" className="hero-image" />
+      </div>
     </div>
   );
 }
