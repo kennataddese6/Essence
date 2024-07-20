b2f0250a9b895267f3bacb26caa92a90932e0b43
unknown <kennataddese6@gmail.com>
2024-07-20 13:30:11 -0700

    add another sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Hero from './ui/hero';
 import { AiOutlineMedicineBox } from 'react-icons/ai';
-import { FaSearchLocation } from 'react-icons/fa';
+import Image from 'next/image';
 import { CiSearch } from 'react-icons/ci';
+import secondImage from '@/assets/secondImage.png';
 export default function Home() {
   return (
     <>
@@ -9,7 +10,7 @@ export default function Home() {
         <Hero />
         <h2 className="text-center my-4">Our services</h2>
         <div className="serviceUnderline"></div>
-        <p className="text-secondary px-2 text-center my-4 col-lg-9 mx-auto">
+        <p className="text-secondary px-3 text-center my-4 col-lg-9 mx-auto">
           We provide the best choices for you. Adjust it to your health needs
           and make sure you under go treatment with our highly qualified doctors
           you can consult with us which type of service is suitable for your
@@ -51,6 +52,25 @@ export default function Home() {
           </div>
         </div>
         <button className="service-button mx-auto my-4">Learn More</button>
+        <div className="heroContainer col-xl-12 col-xxl-9">
+          <div className="col-lg-6 ">
+            <Image src={secondImage} alt="Hero Image" className="hero-image" />
+          </div>
+          <div className=" col-lg-6 herotextcontainer">
+            <h1 className="mx-5 text-center text-lg-start ">
+              Leading healthcare providers
+            </h1>
+            <p className="mx-3 mx-lg-5  text-secondary text-center text-lg-start py-2">
+              We are dedicated to enhancing healthcare by providing top-tier
+              medical equipment from around the globe.Our commitment to
+              excellence ensures that healthcare providers have access to the
+              latest reliable tools.
+            </p>
+            <button className="service-button my-2 w-30 py-2 mx-5">
+              Learn more
+            </button>
+          </div>
+        </div>
         <h1>Hello3</h1>
         <h1>Hello4</h1>
         <h1>Hello</h1>
