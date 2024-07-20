8f761b2bf5dcee3d111459ed13e78ade9a425c93
unknown <kennataddese6@gmail.com>
2024-07-20 13:05:56 -0700

    add service sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,56 @@
-import Hero from "./ui/hero";
+import Hero from './ui/hero';
+import { AiOutlineMedicineBox } from 'react-icons/ai';
+import { FaSearchLocation } from 'react-icons/fa';
+import { CiSearch } from 'react-icons/ci';
 export default function Home() {
   return (
     <>
-      <div className="col-lg-11 col-xl-11 col-xxl-9" style={{ margin: "auto" }}>
+      <div className="col-lg-11 col-xl-11 col-xxl-9 mx-auto">
         <Hero />
-        <h1>Hello2</h1>
+        <h2 className="text-center my-4">Our services</h2>
+        <div className="serviceUnderline"></div>
+        <p className="text-secondary px-3 text-center my-4 col-lg-9 mx-auto">
+          We provide the best choices for you. Adjust it to your health needs
+          and make sure you under go treatment with our highly qualified doctors
+          you can consult with us which type of service is suitable for your
+          health
+        </p>
+        <div className="serviceContainer my-2">
+          <div className="py-3">
+            <CiSearch
+              fontSize={66}
+              color="dodgerblue"
+              className="d-block mx-auto"
+            />
+            <h5 className="text-center m-2 ">Search Medicine</h5>
+            <p className="px-3 text-center text-secondary">
+              Choose your medicine from thousands of general and trusted brands
+            </p>
+          </div>
+          <div className="py-3">
+            <AiOutlineMedicineBox
+              fontSize={66}
+              color="dodgerblue"
+              className="d-block mx-auto"
+            />
+            <h5 className="text-center m-2 ">Search Medicine</h5>
+            <p className="px-3 text-center text-secondary">
+              Choose your medicine from thousands of general and trusted brands
+            </p>
+          </div>
+          <div className="py-3">
+            <AiOutlineMedicineBox
+              fontSize={66}
+              color="dodgerblue"
+              className="d-block mx-auto"
+            />
+            <h5 className="text-center m-2 ">Search Medicine</h5>
+            <p className="px-3 text-center text-secondary">
+              Choose your medicine from thousands of general and trusted brands
+            </p>
+          </div>
+        </div>
+        <button className="service-button mx-auto my-4">Learn More</button>
         <h1>Hello3</h1>
         <h1>Hello4</h1>
         <h1>Hello</h1>
