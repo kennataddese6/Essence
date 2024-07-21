1a177b7c2dfecdeca2185d1d977fdd93a5e34364
unknown <kennataddese6@gmail.com>
2024-07-21 14:47:55 -0700

    add feed

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import secondImage from '@/assets/secondImage.png';
 import thirdImage from '@/assets/thirdImage.png';
 import Services from './ui/services';
+import perfectface from '@/assets/perfect-face.jpg';
 export default function Home() {
   return (
     <>
@@ -49,17 +50,36 @@ export default function Home() {
             <Image src={thirdImage} alt="Hero Image" className="hero-image" />
           </div>
         </div>
-        <div className="col-lg-9 mx-auto py-4 feedbacks">
+        <div className="col-lg-10 mx-auto my-4 py-5 feedbacks">
           <h2 className="text-center text-white">
             What people are saying about us
           </h2>
           <div
-            className="serviceUnderline mx-5 my-5 mx-auto"
+            className="serviceUnderline mx-5  my-5 mx-auto"
             style={{ backgroundColor: 'white !important' }}
           ></div>
-          <div className="d-flex justify-content-between col-lg-9 mx-auto ">
-            <div>Kenna T.</div>
-            <p>"This guys are the deal. I have received the best attibute"</p>
+          <div className="d-flex flex-wrap justify-content-between col-lg-9 mx-auto ">
+            <div className="text-white  d-flex align-items-center  col-lg-6 py-2">
+              <div className="customerProfile">
+                <Image
+                  src={perfectface}
+                  width={100}
+                  alt="profile"
+                  className="customerImage"
+                />
+              </div>
+              <div className="mx-4">
+                <h4 className="my-1">Kenna Taddese</h4>
+                <p>Co-founder</p>
+              </div>
+            </div>
+            <div className="col-lg-6 p-3">
+              <p className="text-white">
+                "This guys are the deal. I have received the best attibute.
+                There patience and growing intel is what makes them the best
+                There patience and growing intel is what makes them the best "
+              </p>
+            </div>
           </div>
         </div>
         <h1>Hello3</h1>
