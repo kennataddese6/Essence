9669fed928f9da65f451f63a097f8845aadcc77c
unknown <kennataddese6@gmail.com>
2024-07-21 01:29:12 -0700

    align button

--- app/ui/hero.tsx
+++ app/ui/hero.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
           equipment from around the globe.Our commitment to excellence ensures
           that healthcare providers have access to the latest reliable tools.
         </p>
-        <button className="btn btn-primary my-2 w-30 py-2">Learn more</button>
+        <button className="btn btn-primary m-2 w-30 py-2">Learn more</button>
       </div>
       <div className="col-lg-6 ">
         <Image src={heroImage} alt="Hero Image" className="hero-image" />
