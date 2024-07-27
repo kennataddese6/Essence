c9bbd2c56553b27ccb0338d12b322881e4fb5f44
unknown <kennataddese6@gmail.com>
2024-07-27 05:33:13 -0700

    animate service

--- app/ui/services.tsx
+++ app/ui/services.tsx
@@ -1,18 +1,42 @@
+'use client';
 import { CiSearch } from 'react-icons/ci';
 import { AiOutlineMedicineBox } from 'react-icons/ai';
 import { FaUserDoctor } from 'react-icons/fa6';
+import { motion } from 'framer-motion';
 export default function Services() {
   return (
     <>
-      <h2 className="text-center my-4">Our services</h2>
-      <div className="serviceUnderline mx-auto"></div>
-      <p className="text-secondary px-3 text-center my-4 col-lg-9 mx-auto">
+      <motion.h2
+        className="text-center my-4"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.5, ease: 'easeIn' }}
+      >
+        Our services
+      </motion.h2>
+      <motion.div
+        className="serviceUnderline mx-auto"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.6, ease: 'easeIn' }}
+      ></motion.div>
+      <motion.p
+        className="text-secondary px-3 text-center my-4 col-lg-9 mx-auto"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.7, ease: 'easeIn' }}
+      >
         We provide the best choices for you. Adjust it to your health needs and
         make sure you under go treatment with our highly qualified doctors you
         can consult with us which type of service is suitable for your health
-      </p>
+      </motion.p>
       <div className="serviceContainer my-2">
-        <div className="py-3">
+        <motion.div
+          className="py-3"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.7, ease: 'easeIn' }}
+        >
           <CiSearch
             fontSize={66}
             color="dodgerblue"
@@ -22,8 +46,13 @@ export default function Services() {
           <p className="px-3 text-center text-secondary">
             Choose your medicine from thousands of general and trusted brands
           </p>
-        </div>
-        <div className="py-3">
+        </motion.div>
+        <motion.div
+          className="py-3"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 1, ease: 'easeIn' }}
+        >
           <FaUserDoctor
             fontSize={66}
             color="dodgerblue"
@@ -33,8 +62,13 @@ export default function Services() {
           <p className="px-3 text-center text-secondary">
             Choose your medicine from thousands of general and trusted brands
           </p>
-        </div>
-        <div className="py-3">
+        </motion.div>
+        <motion.div
+          className="py-3"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 1.3, ease: 'easeIn' }}
+        >
           <AiOutlineMedicineBox
             fontSize={66}
             color="dodgerblue"
@@ -44,9 +78,16 @@ export default function Services() {
           <p className="px-3 text-center text-secondary">
             Choose your medicine from thousands of general and trusted brands
           </p>
-        </div>
+        </motion.div>
       </div>
-      <button className="service-button mx-auto my-4">Learn More</button>
+      <motion.button
+        className="service-button mx-auto my-4"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 1, ease: 'easeIn' }}
+      >
+        Learn More
+      </motion.button>
     </>
   );
 }
