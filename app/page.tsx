fee1394ecf44dcac8231b1e1fbb711b92bc39b6d
kennataddese6 <kennataddese6@gmail.com>
2024-11-02 14:29:10 +0300

    separate products

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Image from 'next/image';
-import Hero from './ui/hero';
-import Services from './ui/services';
-import Benefits from './ui/benefits';
-import Feedbacks from './ui/feedbacks';
-import eqip1 from '@/assets/equip1.jpg';
-import eqip2 from '@/assets/equip2.jpg';
-import eqip3 from '@/assets/equip3.jpg';
-import eqip4 from '@/assets/equip4.webp';
-import { FaStar } from 'react-icons/fa';
+import Hero from "./ui/hero";
+import Services from "./ui/services";
+import Benefits from "./ui/benefits";
+import Feedbacks from "./ui/feedbacks";
+import Products from "./ui/products";
+
 export default function Home() {
   return (
     <>
@@ -16,110 +12,20 @@ export default function Home() {
         <Services />
         <Benefits />
         <Feedbacks />
-        <h2 className="text-center mt-5 mb-4">Check our latest Products</h2>
-        <div
-          className="serviceUnderline mx-auto mb-5"
-          style={{ background: 'black' }}
-        ></div>
-        <div className="d-flex justify-content-around flex-wrap productsContainer">
+        <Products />
+        <div className="bg-primary flex flex-column">
+          <div className="w-50 bg-warning">Where we Reside</div>
           <div>
-            <Image
-              src={eqip1}
-              alt="product image"
-              width={250}
-              height={150}
-              className="productImage"
-            />
-            <h5 className="text-center bold-text m-1">Thermocare</h5>
-            <p className="text-secondary text-center my-1 mx-2">
-              <small>
-                This is the best equiment now. Take it without hesitation
-              </small>
-            </p>
-            <p className="text-center m-1 bold-text">$499</p>
-            <p className="text-center  w-50 d-flex mx-auto justify-content-around px-2">
-              <FaStar color="gold" />
-              <FaStar color="gold" />
-              <FaStar color="gold" />
-              <FaStar color="gold" />
-              <FaStar />
-            </p>
-            <button className="order-button">Buy Now</button>
-          </div>
-          <div>
-            <Image
-              src={eqip2}
-              alt="product image"
-              width={250}
-              height={150}
-              className="productImage"
-            />
-            <h5 className="text-center bold-text m-1">Microbiaz</h5>
-            <p className="text-secondary text-center my-1 mx-2">
-              <small>
-                This is the best equiment now. Take it without hesitation
-              </small>
-            </p>
-            <p className="text-center m-1 bold-text">$899</p>
-            <p className="text-center  w-50 d-flex mx-auto justify-content-around px-2">
-              <FaStar color="gold" />
-              <FaStar color="gold" />
-              <FaStar color="gold" />
-              <FaStar />
-              <FaStar />
-            </p>
-            <button className="order-button">Buy Now</button>
-          </div>
-          <div>
-            <Image
-              src={eqip3}
-              alt="product image"
-              width={250}
-              height={150}
-              className="productImage"
-            />
-            <h5 className="text-center bold-text m-1">Lepu</h5>
-            <p className="text-secondary text-center my-1 mx-2">
-              <small>
-                This is the best equiment now. Take it without hesitation
-              </small>
-            </p>
-            <p className="text-center m-1 bold-text">$799</p>
-            <p className="text-center  w-50 d-flex mx-auto justify-content-around px-2">
-              <FaStar color="gold" />
-              <FaStar color="gold" />
-              <FaStar color="gold" />
-              <FaStar color="gold" />
-              <FaStar color="gold" />
-            </p>
-            <button className="order-button">Buy Now</button>
-          </div>
-          <div>
-            <Image
-              src={eqip4}
-              alt="product image"
-              width={250}
-              height={150}
-              className="productImage"
-            />
-            <h5 className="text-center bold-text m-1">Shenzhen</h5>
-            <p className="text-secondary text-center my-1 mx-2">
-              <small>
-                This is the best equiment now. Take it without hesitation
-              </small>
-            </p>
-            <p className="text-center m-1 bold-text">$699</p>
-            <p className="text-center  w-50 d-flex mx-auto justify-content-around px-2">
-              <FaStar color="gold" />
-              <FaStar color="gold" />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-            </p>
-            <button className="order-button">Buy Now</button>
+            here
+            {/*<iframe
+              className="w-50"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.8311494583613!2d38.787831774066376!3d8.987680189636338!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b853c14f0e2d5%3A0x32ce0404870c8088!2sEthiopian%20Skylight%20Hotel!5e0!3m2!1sen!2set!4v1730382389754!5m2!1sen!2set"
+              height="450"
+              allowFullScreen
+              loading="lazy"
+            ></iframe> */}
           </div>
         </div>
-        <button className="service-button mx-auto my-5">View More</button>
       </div>
     </>
   );
---
+++ app/ui/products.tsx
@@ -0,0 +1,116 @@
+import Image from "next/image";
+import eqip1 from "@/assets/equip1.jpg";
+import eqip2 from "@/assets/equip2.jpg";
+import eqip3 from "@/assets/equip3.jpg";
+import eqip4 from "@/assets/equip4.webp";
+import { FaStar } from "react-icons/fa";
+export default function Products() {
+  return (
+    <>
+      <h2 className="text-center mt-5 mb-4">Check our latest Products</h2>
+      <div
+        className="serviceUnderline mx-auto mb-5"
+        style={{ background: "black" }}
+      ></div>
+      <div className="d-flex justify-content-around flex-wrap productsContainer">
+        <div>
+          <Image
+            src={eqip1}
+            alt="product image"
+            width={250}
+            height={150}
+            className="productImage"
+          />
+          <h5 className="text-center bold-text m-1">Thermocare</h5>
+          <p className="text-secondary text-center my-1 mx-2">
+            <small>
+              This is the best equiment now. Take it without hesitation
+            </small>
+          </p>
+          <p className="text-center m-1 bold-text">$499</p>
+          <p className="text-center  w-50 d-flex mx-auto justify-content-around px-2">
+            <FaStar color="gold" />
+            <FaStar color="gold" />
+            <FaStar color="gold" />
+            <FaStar color="gold" />
+            <FaStar />
+          </p>
+          <button className="order-button">Buy Now</button>
+        </div>
+        <div>
+          <Image
+            src={eqip2}
+            alt="product image"
+            width={250}
+            height={150}
+            className="productImage"
+          />
+          <h5 className="text-center bold-text m-1">Microbiaz</h5>
+          <p className="text-secondary text-center my-1 mx-2">
+            <small>
+              This is the best equiment now. Take it without hesitation
+            </small>
+          </p>
+          <p className="text-center m-1 bold-text">$899</p>
+          <p className="text-center  w-50 d-flex mx-auto justify-content-around px-2">
+            <FaStar color="gold" />
+            <FaStar color="gold" />
+            <FaStar color="gold" />
+            <FaStar />
+            <FaStar />
+          </p>
+          <button className="order-button">Buy Now</button>
+        </div>
+        <div>
+          <Image
+            src={eqip3}
+            alt="product image"
+            width={250}
+            height={150}
+            className="productImage"
+          />
+          <h5 className="text-center bold-text m-1">Lepu</h5>
+          <p className="text-secondary text-center my-1 mx-2">
+            <small>
+              This is the best equiment now. Take it without hesitation
+            </small>
+          </p>
+          <p className="text-center m-1 bold-text">$799</p>
+          <p className="text-center  w-50 d-flex mx-auto justify-content-around px-2">
+            <FaStar color="gold" />
+            <FaStar color="gold" />
+            <FaStar color="gold" />
+            <FaStar color="gold" />
+            <FaStar color="gold" />
+          </p>
+          <button className="order-button">Buy Now</button>
+        </div>
+        <div>
+          <Image
+            src={eqip4}
+            alt="product image"
+            width={250}
+            height={150}
+            className="productImage"
+          />
+          <h5 className="text-center bold-text m-1">Shenzhen</h5>
+          <p className="text-secondary text-center my-1 mx-2">
+            <small>
+              This is the best equiment now. Take it without hesitation
+            </small>
+          </p>
+          <p className="text-center m-1 bold-text">$699</p>
+          <p className="text-center  w-50 d-flex mx-auto justify-content-around px-2">
+            <FaStar color="gold" />
+            <FaStar color="gold" />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </p>
+          <button className="order-button">Buy Now</button>
+        </div>
+      </div>
+      <button className="service-button mx-auto my-5">View More</button>
+    </>
+  );
+}
