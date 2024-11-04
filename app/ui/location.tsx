da89efc8c0e2cd48cbccdd920c8af2cdee959d71
kennataddese6 <kennataddese6@gmail.com>
2024-11-04 15:22:12 +0300

    add location

--- app/ui/location.tsx
+++ app/ui/location.tsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion";
 export default function Location() {
   return (
-    <div className="flex flex-wrap-reverse my-5">
+    <div className="flex flex-wrap-reverse my-5 mx-4">
       <div className="w-full lg:w-1/2 rounded-3xl">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.8311494583613!2d38.787831774066376!3d8.987680189636338!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b853c14f0e2d5%3A0x32ce0404870c8088!2sEthiopian%20Skylight%20Hotel!5e0!3m2!1sen!2set!4v1730382389754!5m2!1sen!2set"
@@ -21,7 +21,12 @@ export default function Location() {
         >
           Where we reside
         </motion.h2>
-        <div className="w-28 h-0.5 bg-black lg:ml-24 lg:mt-4 bg-gradient-to-r from-green-400 to-blue-500"></div>
+        <div className="my-4 ml-auto mr-auto w-28 h-0.5 bg-black lg:ml-24 lg:mr-0 bg-gradient-to-r from-green-400 to-blue-500"></div>
+        <p className="lg:ml-24 lg:text-start">
+          {" "}
+          Our address in bole in front of Ethiopian airlines on the first left
+          to Civil Avaiation in Addis Ababa, Ethiopia
+        </p>
       </div>
     </div>
   );
