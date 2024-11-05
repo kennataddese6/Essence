5cc35db423eb28eb866837db7f2998cb9ec8b5a4
kennataddese6 <kennataddese6@gmail.com>
2024-11-05 19:18:00 +0300

    prepare a category form

--- app/(admin)/category/page.tsx
+++ app/(admin)/category/page.tsx
@@ -1,3 +1,29 @@
 export default function Page() {
-  return <div>Category</div>;
+  return (
+    <div className="m-4">
+      <form
+        action=""
+        className="py-6 px-14 border border-gray-500 text-center rounded-md"
+      >
+        <label htmlFor="category" className="font-bold text-2xl">
+          Add Category
+        </label>
+        <br />
+        <input
+          type="text"
+          placeholder="Enter category name"
+          id="category"
+          required
+          className="mt-4 mb-2 border border-gray-500 py-2 px-3 w-52"
+        />
+        <br />
+        <button
+          type="submit"
+          className="bg-slate-900 text-white py-2 px-8 w-52"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
