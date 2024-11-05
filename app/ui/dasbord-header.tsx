5a64435acdd09fca781c41a8b5241b06e344e84f
kennataddese6 <kennataddese6@gmail.com>
2024-11-05 19:02:41 +0300

    format url for dasboard header

--- app/ui/dasbord-header.tsx
+++ app/ui/dasbord-header.tsx
@@ -3,6 +3,8 @@ import { usePathname } from "next/navigation";
 export default function DasboardHeader() {
   const pathname = usePathname();
   return (
-    <h1 className="font-bold text-2xl py-2 px-4">{pathname.split("/")}</h1>
+    <h1 className="font-bold text-2xl py-2 px-4">
+      {pathname.charAt(1).toUpperCase() + pathname.slice(2)}
+    </h1>
   );
 }
