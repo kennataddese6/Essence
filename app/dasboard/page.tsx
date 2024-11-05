5643175f365803148360b82d234b018bb397426c
kennataddese6 <kennataddese6@gmail.com>
2024-11-05 18:38:58 +0300

    add more dashboad content

--- app/dasboard/page.tsx
+++ app/dasboard/page.tsx
@@ -1,8 +1,14 @@
 export default function Page() {
   return (
-    <div className="bg-slate-900 text-white text-center py-4 px-20 m-4 rounded-lg">
-      <h1 className="text-6xl">24</h1>
-      <p>Products</p>
+    <div className="flex flex-wrap">
+      <div className="bg-slate-900 text-white text-center py-4 px-20 m-4 rounded-lg">
+        <h1 className="text-6xl">24</h1>
+        <p>Products</p>
+      </div>
+      <div className="bg-slate-900 text-white text-center py-4 px-20 m-4 rounded-lg">
+        <h1 className="text-6xl">5</h1>
+        <p>Categories</p>
+      </div>
     </div>
   );
 }
