8165b0f63cdcab4e976b309aa09349b4c66a9c10
kennataddese6 <kennataddese6@gmail.com>
2024-11-09 21:28:26 +0300

    create an api that returns products image

---
+++ app/image/[id]/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest } from "next/server";
+import path from "path";
+import fs from "fs";
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  try {
+    const imagePath = path.join(process.cwd(), `storage/images/${id}.WebP`);
+    const imageBuffer = fs.readFileSync(imagePath);
+    return new Response(imageBuffer, {
+      headers: {
+        "Content-Type": "image/webp",
+      },
+    });
+  } catch (error) {
+    return new Response("Image not found", {
+      status: 404,
+    });
+  }
+}
