bebe8cc2e9383f3fe716f3f93bd30e5e939e7a98
kennataddese6 <kennataddese6@gmail.com>
2024-11-10 13:12:00 +0300

    remvoe pulse

--- app/(admin)/components/category-table.tsx
+++ app/(admin)/components/category-table.tsx
@@ -22,7 +22,7 @@ export default async function CategoryTable() {
             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white animate-pulse"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
                 {category.name}
               </th>
