9c4bf16a838a40003a229a7cc7c91e913aa7947a
kennataddese6 <kennataddese6@gmail.com>
2024-11-10 13:17:37 +0300

    add skeleton for categories

--- app/(admin)/category/page.tsx
+++ app/(admin)/category/page.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from "react";
 import CategoryTable from "../components/category-table";
 import CategoryFormUI from "../ui/category-form-ui";
+import { CategoriesTableSkeleton } from "../components/category-table-skeleton";
 
 export default function Page() {
   return (
     <div className="m-4">
       <CategoryFormUI />
-      <CategoryTable />
+      <Suspense fallback={<CategoriesTableSkeleton />}>
+        <CategoryTable />
+      </Suspense>
     </div>
   );
 }
---
+++ app/(admin)/components/category-table-skeleton.tsx
@@ -0,0 +1,31 @@
+export async function CategoriesTableSkeleton() {
+  return (
+    <div className="relative overflow-x-auto mt-4">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Category name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              ID
+            </th>
+            <th scope="col" className="px-6 py-3"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white animate-pulse"
+            >
+              loading
+            </th>
+            <td className="px-6 py-4"></td>
+            <td className="px-6 py-4"></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
