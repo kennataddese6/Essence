9e017172d8c202c819037444ab89e14509ddcb67
kennataddese6 <kennataddese6@gmail.com>
2024-11-10 12:46:31 +0300

    add supense to product table

--- app/(admin)/product/page.tsx
+++ app/(admin)/product/page.tsx
@@ -2,13 +2,16 @@ import { Suspense } from "react";
 import { ProductsTable } from "../components/products-table";
 import { ProductsForm } from "../components/products-form";
 import ProductsFormSkeleton from "../components/product-form-skeleton";
+import { ProductsTableSkeleton } from "../components/Product-table-skeleton";
 export default function Page() {
   return (
     <div className="m-4">
       <Suspense fallback={<ProductsFormSkeleton />}>
         <ProductsForm />
       </Suspense>
-      <ProductsTable />
+      <Suspense fallback={<ProductsTableSkeleton />}>
+        <ProductsTable />
+      </Suspense>
     </div>
   );
 }
