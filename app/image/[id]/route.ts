8e959e5688283912058eab0e2661c625bf64b153
kennataddese6 <kennataddese6@gmail.com>
2025-04-16 09:46:18 +0300

    add bot

--- app/image/[id]/route.ts
+++ app/image/[id]/route.ts
@@ -1,22 +1,22 @@
-import path from "path";
-import fs from "fs";
+import fs from "fs"
+import path from "path"
 export async function GET(
   req: any,
   res: any,
-  { params }: { params: { id: string } }
+  { params }: { params: { id: string } },
 ) {
-  const { id } = params;
+  const { id } = params
   try {
-    const imagePath = path.join(process.cwd(), `storage/images/${id}.WebP`);
-    const imageBuffer = fs.readFileSync(imagePath);
+    const imagePath = path.join(process.cwd(), `storage/images/${id}.WebP`)
+    const imageBuffer = fs.readFileSync(imagePath)
     return new Response(imageBuffer, {
       headers: {
         "Content-Type": "image/webp",
       },
-    });
+    })
   } catch (error) {
     return new Response("Image not found", {
       status: 404,
-    });
+    })
   }
 }
